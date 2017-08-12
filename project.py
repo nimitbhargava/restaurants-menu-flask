cd37fc35527085bc6c35da0bde923772410b12aa
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-12 15:12:58 +0530

    Routes using decorators

--- project.py
+++ project.py
@@ -28,6 +28,18 @@ def RestaurantMenu(restaurant_id):
         output += '</br>'
     return output
 
+@app.route('/restaurants/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
