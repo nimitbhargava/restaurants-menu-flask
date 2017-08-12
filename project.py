3252dd7daf902049ff43aadb2b937f23e5072f4c
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-12 12:45:44 +0530

    Imported SQLAlchemy and database engine in sessionmaker

--- project.py
+++ project.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 app = Flask(__name__)
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 @app.route("/")
 @app.route("/hello")
